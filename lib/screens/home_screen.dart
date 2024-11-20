5f3f3cf46991470f8129712d3aff6cea2664fb8d
MohsenRahmdeli <mohsen.rahmdeli@gmail.com>
2024-11-20 11:26:17 +0400

    Create HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text(
+            'Reminder',
+            style: TextStyle(
+              color: Colors.teal,
+            ),
+          ),
+          iconTheme: IconThemeData(
+            color: Colors.teal,
+          ),
+        ),
+      ),
+    );
   }
 }
